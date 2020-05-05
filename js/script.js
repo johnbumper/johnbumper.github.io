942df8b39a1a69f67e4fd51823e8a37f71e086e6
johnbumper <64822316+johnbumper@users.noreply.github.com>
2020-05-05 16:29:47 -0500

    update

--- js/script.js
+++ js/script.js
@@ -3,20 +3,22 @@ angular.module('ngRouteExample', ['ngRoute'])
 .controller('ChuController', function ($scope, $route) { $scope.$route = $route;})
 .controller('BumperController', function ($scope, $route) { $scope.$route = $route;})
 .config(function ($routeProvider) {
-	$routeProvider.
-	when('/home', {
-		templateUrl: 'index.html',
-		controller: 'HomeController'
-	}).
-	when('/xcc', {
-		templateUrl: 'chu.html',
-		controller: 'ChuController'
-	}).
-	when('/bumper', {
-		templateUrl: 'bumper.html',
-		controller: 'BumperController'
-	}).
-	otherwise({
-		redirectTo: '/home'
-	});
+    $routeProvider.
+    when('/home', {
+        templateUrl: 'home.html',
+        controller: 'HomeController'
+    }).
+    when('/chu', {
+        templateUrl: 'chu.html',
+        controller: 'ChuController'
+    }).
+    when('/bumper', {
+        templateUrl: 'bumper.html',
+        controller: 'BumperController'
+    }).
+    otherwise({
+        redirectTo: '/home'
+    });
 });
+
+angular.module('myApp', ['ngAnimate']);
